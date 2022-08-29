f9332ad99ae494fcbe189a0958d3f67dbbe18296
Rob Sargent <rob.sargent@utah.edu>
2022-08-29 09:58:10 -0600

    using file actually produced in Step1(x_cumc_patient_matched) sql99

--- Step2_Relationship_Inference/1_exclude_EC_w_most_matches.sql
+++ Step2_Relationship_Inference/1_exclude_EC_w_most_matches.sql
@@ -1,18 +1,21 @@
 
 
-### Exclude spouses and self 
-create table database.matches_wo_spouse
-select a.mrn, l.relationship_group, a.relation_mrn, a.matched_path
-from database.pt_matches a
-join database.relationship_lookup l using (relationship)
-where relationship_group != ''
-and relationship_group != 'Spouse'
-and mrn != relation_mrn;
+--### Exclude spouses and self 
+-- create table 
+insert into cell.matches_wo_spouse
+select a.empi_or_mrn as mrn, l.relationship_group, a.relation_empi_or_mrn as relation_mrn, a.matched_path
+from cell.x_cumc_patient_matched a --cell.pt_matches ab
+join cell.relationship_lookup l on a.relationship = l.relationship
+where l.relationship_group != ''
+      and relationship_group != 'Spouse'
+      and a.empi_or_mrn != a.relation_empi_or_mrn
+;
 
-### Exclude emergency contacts with most matches 
-create table database.ec_exclude
+--### Exclude emergency contacts with most matches 
+create table cell.ec_exclude as
 select relation_mrn, count(distinct m.mrn), demog.*
-from matches_wo_spouse m
-join pt_demog demog on (demog.mrn = relation_mrn)
+from cell.matches_wo_spouse m
+join cell.pt_demog demog on (demog.mrn = relation_mrn)
 group by relation_mrn
-having count(distinct.mrn) >= 20;
+having count(distinct empi_or_mrn) >= 20
+;
