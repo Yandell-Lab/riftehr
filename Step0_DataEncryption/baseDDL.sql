6ab9aba2079a7b445cf5a032c8af1f9cf21207f9
Rob Sargent <rob.sargent@utah.edu>
2022-10-17 17:30:53 -0600

    mrn is text

--- Step0_DataEncryption/baseDDL.sql
+++ Step0_DataEncryption/baseDDL.sql
@@ -2,7 +2,7 @@ create schema if not exists cell;
 
 drop table if exists x_ec_processed;
 create table x_ec_processed (
- mrn_1 int, ec_firstname text, ec_lastname text, ec_phonenumber text, ec_zipcode text, ec_relationship text
+ mrn_1 text, ec_firstname text, ec_lastname text, ec_phonenumber text, ec_zipcode text, ec_relationship text
 );
 
 drop table if exists x_pt_processed;
