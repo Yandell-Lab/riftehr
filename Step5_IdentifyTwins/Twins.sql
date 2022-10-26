3149fe6bd78acb733b681f4fab87d5bcdb145fd7
Rob Sargent <rob.sargent@utah.edu>
2022-10-26 10:49:25 -0600

    point at correct? table

--- Step5_IdentifyTwins/Twins.sql
+++ Step5_IdentifyTwins/Twins.sql
@@ -5,15 +5,14 @@ select distinct
        rel.mrn
        , rel.relation_mrn
        , rel.relationship
-       , demog.year as mrn_dob
-       , demog2.year as relation_dob
-from actual_and_inf_rel_clean_final rel
-join pt_demog demog on rel.mrn = demog.mrn
-join pt_demog demog2 on rel.relation_mrn = demog2.mrn
+       , d.year as mrn_dob
+       , e.year as relation_dob
+--from actual_and_inf_rel_clean_final rel
+from actual_and_inf_rel_part2_unique_clean rel
+join pt_demog d on rel.mrn = d.mrn
+join pt_demog e on rel.relation_mrn = e.mrn
 where rel.relationship = 'Sibling'
 \p\g
-
-create index on actual_and_inf_rel_clean_final(mrn)\p\g
 
 -- # select siblings with the same DOB 
 select distinct *
@@ -23,13 +22,14 @@ where mrn_dob = relation_dob\p\g
 -- UT: find sibs without parents or children
 -- UT: in the 100K test suite, zero sibs are parents or children
 Siblings with no other connection\p\r
-
+/* ON HOLD
 select s.* 
-from actual_and_inf_rel_clean_final s
+from actual_and_inf_rel_part2_unique_clean s
 where s.relationship = 'Sibling' 
       and not exists (select 1 from actual_and_inf_rel_clean_final p 
                       where p.relationship = 'Child' and (s.mrn = p.mrn or s.mrn = p.relation_mrn))
       and not exists (select 1 from actual_and_inf_rel_clean_final p 
                      where p.relationship = 'Child' and (s.relation_mrn = p.mrn or s.relation_mrn = p.relation_mrn))
+*/
 \p\g                     
 
