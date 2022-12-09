42d2ce60443b0705205a9b5b71e1e51d039619bd
Rob Sargent <rob.sargent@utah.edu>
2022-12-09 14:55:05 -0700

    use the "cell" account

--- Step0_DataEncryption/runSteps.sh
+++ Step0_DataEncryption/runSteps.sh
@@ -34,19 +34,17 @@ if [[ -n $STOPSET ]]; then echo Stopping at $STOPSET; fi
 
 stager 1.1 "load data, find match $STEPDIR"
 
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
-set search_path = cell, run, public;
+set search_path = cell, run, public; 
 \i $STEPDIR/find_matches.sql
 EOF
 
 
 STEPDIR=$RIFTDIR/Step2_Relationship_Inference
 stager 2.1 "generate opposites $STEPDIR"
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 \i $STEPDIR/1_exclude_EC_w_most_matches.sql
 \i $STEPDIR/2_clean_up_BEFORE_inferring_relationships.sql
@@ -58,9 +56,8 @@ time julia $STEPDIR/3_Infer_Relationships.jl $DATADIR
 wc -l $DATADIR/output_actual_and_inferred_relationships.csv
 
 stager 2.4 "clean-up post julia"
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 \copy actual_and_inf_rel_part1 from $DATADIR/output_actual_and_inferred_relationships.csv csv
 \i $STEPDIR/4_clean_up_after_inferences_part1.sql
@@ -71,10 +68,10 @@ stager 2.5  "families (julia)"
 julia $STEPDIR/5_Infer_Relationships_part2.jl $DATADIR
 
 stager 2.6 "clean up again"
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
+select count(*) as n4_actual_and_inf_rel_part2 from actual_and_inf_rel_part2\p\g
 \copy actual_and_inf_rel_part2 from $DATADIR/output_patient_relations_w_opposites_part2.csv csv
 \i $STEPDIR/6_clean_up_after_inferences_part2.sql
 EOF
@@ -84,9 +81,8 @@ ml python/3.9.7
 
 STEPDIR=$RIFTDIR/Step3_AssignFamilyIDs
 stager 3.1 "generate family"
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 \i $STEPDIR/create_table_to_generate_family_id.sql
 \copy all_relationships_to_generate_family_id to $DATADIR/all_relationship.csv csv
@@ -96,21 +92,20 @@ time sort -u $DATADIR/all_relationship.csv > $DATADIR/all_relationship.sorted.cs
 stager 3.2 "python family id generator"
 python3 $STEPDIR/All_relationships_family_ID.py $DATADIR/all_relationship.sorted.csv $DATADIR/all_families.csv
 
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 -- I don't see this table def, import anywhere...
 drop table if exists family_ids\p\g
 create table family_ids(family_id int, individual_id text)\p\g
 \copy family_ids from $DATADIR/all_families.csv csv header
+copy family_ids from $DATADIR/all_families.csv csv header\p\r
 EOF
 
 STEPDIR=$RIFTDIR/Step4_ConflictingRelationships
 stager 4.1 "Conflict resolution"
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 \i $STEPDIR/identify_conflicting_relationships_with_family_id.sql
 EOF
@@ -118,9 +113,8 @@ EOF
 STEPDIR=$RIFTDIR/Step5_IdentifyTwins
 stager 5.1 "Twins"
 
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 \i $STEPDIR/Twins.sql
 EOF
@@ -129,14 +123,14 @@ stager 6.1 "No-op"
 STEPDIR=$RIFTDIR/Step7_EMP
 stager 7.1 "EMP"
 
-psql --user postgres --dbname postgres --host csgsdb <<EOF
+psql --user cell --dbname riftehr --host csgsdb <<EOF
 \timing on
-\c riftehr
 set search_path = cell, run, public;
 
 drop table if exists all_relationship_s7;
 create table all_relationship_s7(mrn text, relation text, relation_mrn text);
 \copy all_relationship_s7 from $DATADIR/all_relationship.sorted.csv csv
+copied all_relationship_s7 from $DATADIR/all_relationship.sorted.csv csv\p\r
 \i $STEPDIR/1_reports.sql
 \i $STEPDIR/2_child_only.sql
 EOF
