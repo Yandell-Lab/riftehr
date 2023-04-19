15e34b17fdf19f615c96f9f1232ef95f66f07c21
Rob Sargent <rob.sargent@utah.edu>
2023-04-19 08:47:31 -0600

    correct incorrect translation of "sum(bool)"

--- Step4_ConflictingRelationships/identify_conflicting_relationships_with_family_id.sql
+++ Step4_ConflictingRelationships/identify_conflicting_relationships_with_family_id.sql
@@ -8,6 +8,13 @@ create unique index on family_ids(individual_id)
 delete from actual_and_inf_rel_clean_final where relationship is null
 \p\g
 
+
+/* This tallying is faulty.  The "actual_and_inf_rel_clean_final" is
+ * actually, finally pretty clean but it does have extra for records
+ * - spouses in twice (a.mrn = b.relation_mrn and b.mrn = a.relation_mrn)
+ * - siblings in twice (a.mrn = b.relation_mrn and b.mrn = a.relation_mrn)
+ * - true conflicts undetected:(a.mrn = b.relation_mrn and b.mrn = a.relation_mrn and a.relationship != b.relationship)
+*/
 drop table if exists actual_and_inf_rel_clean_final_count_rels\p\g
 create table actual_and_inf_rel_clean_final_count_rels as
 select f.family_id, a.mrn, relation_mrn, count(distinct relationship) as num_uniq_rels
@@ -16,11 +23,14 @@ join actual_and_inf_rel_clean_final a on f.individual_id = a.mrn
 group by a.mrn, a.relation_mrn, f.family_id
 \p\g
 
+
+/* The penchant for summing booleans was at play here.*/
 drop table if exists family_ids_count_conflicted\p\g
 create table family_ids_count_conflicted as
 select family_id
        , count(distinct mrn) as num_individuals
-       , sum(num_uniq_rels) as num_rels_conflicted
+       , sum(case when num_uniq_rels = 1 then 0 else 1 end) as num_rels_conflicted
 from actual_and_inf_rel_clean_final_count_rels
 group by family_id
-having sum(num_uniq_rels) > 1\p\g
+\p\g
+
